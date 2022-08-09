ed1f5fd91cc950399ae6f0f123560edd0152af24
Feruz00 <feruzatamyradow@mail.ru>
2022-08-09 22:54:23 +0500

    change hello world text

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 const Home = () => (
   <div>
     <h1 className="text-3xl underline text-bold cursor-pointer absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
-      Hello
+      Hello world
     </h1>
   </div>
 );
