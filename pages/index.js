d298eee63a970e0851dad7b3a67767d496a725fd
Feruz00 <feruzatamyradow@mail.ru>
2022-08-09 22:28:28 +0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+
+const Home = () => (
+  <div>
+    <h1 className="text-3xl underline text-bold cursor-pointer absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
+      Hello
+    </h1>
+  </div>
+);
+
+export default Home;
