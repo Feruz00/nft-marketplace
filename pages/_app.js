5f5faa888c7a066f385d3d14a3acacbe1be5b829
Feruz00 <feruzatamyradow@mail.ru>
2022-08-10 22:22:37 +0500

    navbar builded

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
+import { Navbar, Footer } from '../components';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/7409f70bd9.js" crossorigin="anonymous" />
+  </ThemeProvider>
+);
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
 
 const Home = () => (
-  <div>
-    <h1 className="text-3xl underline text-bold cursor-pointer absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
-      Hello world
-    </h1>
-  </div>
+  <div />
 );
 
 export default Home;
